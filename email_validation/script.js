45558968d8eb15cf5bcafd67eb5045e957507d56
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-05-21 19:01:48 +0300

    email validator question

---
+++ email_validation/script.js
@@ -0,0 +1,10 @@
+
+// It must have the username@websitename.extension format type.
+// The maximum length of the extension is 3.
+// Write a function that takes email variable and return true or false and write console invalid or valid.
+// Please note that, extension length can be 2 or 3 letters( For example: .co, com. )
+// Pseudocode:
+// includes @
+// if ext > 3 --> false
+// 
+
